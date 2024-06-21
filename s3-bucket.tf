9343cc78ff132d9815b0b02fa9de1bc668ed033b
Alberto Sagrado <asagrado@amazon.com>
2024-06-21 15:36:07 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
   source  = "app.terraform.io/terraform-training-organization-asagrado/s3-bucket/aws"
-  version = "2.8.0"
+  version = "4.1.2"
   bucket_prefix = "asagrado"
 }
